6cfb0f128d7b9ad9367ff918de0304c527ad740f
Dan Geffroy <ogzmovie@gmail.com>
2023-06-26 17:24:44 +0200

    feat: adding blobby the cool blob cursor

---
+++ src/components/Cursor/CursorInit.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { Cursor } from "./Cursor";
+  onMount(() => {
+    new Cursor({
+      targets: ["a", ".title", "#header"],
+      count: 6,
+    });
+  });
+</script>
