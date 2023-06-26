6d60dcfedf1e5d095b824142378f1ab5957162ce
Dan Geffroy <ogzmovie@gmail.com>
2023-06-26 17:37:53 +0200

    fix: removing blobby on mobile

--- src/components/Cursor/CursorInit.svelte
+++ src/components/Cursor/CursorInit.svelte
@@ -2,9 +2,15 @@
   import { onMount } from "svelte";
   import { Cursor } from "./Cursor";
   onMount(() => {
-    new Cursor({
-      targets: ["a", ".title", "#header"],
-      count: 6,
-    });
+    if (
+      !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      new Cursor({
+        targets: ["a", ".title", "#header"],
+        count: 6,
+      });
+    }
   });
 </script>
